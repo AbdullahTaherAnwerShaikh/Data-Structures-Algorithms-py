8a8db1700557cd52972cbdea9aacc90ec7ad04d3
Abdullah Taher Anwer Shaikh <abdullah.taher810@gmail.com>
2026-07-13 19:54:09 +0300

    Two Sum ; Easy : Array ; Optimal

---
+++ Two Sum (O).py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numMap = {}
+        n = len(nums)
+        
+        for i in range(n):
+            comp = target - nums[i]
+            if comp in numMap and numMap[comp] != i:
+                return [i,numMap[comp]]
+            else:
+                numMap [nums[i]] = i
+
+        return []
